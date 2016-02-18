a32327fc253f1324b1502ccaf3b08421689bffe8
Sergio Galindo <sergio.e.galindo@gmail.com>
2016-02-18 20:18:19 +0100

    WAR for nvidia GL detection. Clean-ups.

--- CMake/FindnvidiaOpenGL.cmake
+++ CMake/FindnvidiaOpenGL.cmake
@@ -2,7 +2,7 @@
 
 find_library(NVIDIA_OPENGL_gl_LIBRARY
   GL
-  PATHS /usr/lib/nvidia-331 /usr/lib/nvidia-340
+  PATHS /usr/lib/nvidia-331 /usr/lib/nvidia-340 /usr/lib/nvidia-352
   NO_DEFAULT_PATH
   NO_SYSTEM_ENVIRONMENT_PATH
 )
@@ -12,3 +12,7 @@ get_filename_component(NVIDIA_OPENGL_gl_LIBRARY_PATH
 ${NVIDIA_OPENGL_gl_LIBRARY} 
 DIRECTORY)
 
+
+if ( NVIDIA_OPENGL_gl_LIBRARY )
+  set(NVIDIAOPENGL_FOUND 1)
+endif( )
